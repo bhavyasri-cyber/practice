ad23ce054fd8b658e9e24f67d641044e32ea9561
bhavyasri-cyber <144777808+bhavyasri-cyber@users.noreply.github.com>
2026-02-03 09:54:52 +0530

    Add files via upload

---
+++ AppController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    private final AppInfoService appInfoService;
+
+    public AppController(AppInfoService appInfoService) {
+        this.appInfoService = appInfoService;
+    }
+
+    @GetMapping("/app")
+    public String getAppInfo() {
+        return appInfoService.getAppInfo();
+    }
+}
